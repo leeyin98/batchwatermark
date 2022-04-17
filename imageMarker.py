e6f334d5fae3ab5cffb092a200db0b50fbacc049
lixj <1542396534@qq.com>
2022-04-17 21:19:26 +0800

    [feat] 新增擦除不规则图形功能（暂未完全实现）

--- imageMarker.py
+++ imageMarker.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 import random
+import cv2
+import numpy as np
 
 from PIL import Image
 
@@ -8,6 +10,11 @@ from PIL import Image
 # 图片水印
 def add_image_mark(image_path, mark_path, args):
     temp_path = "./temp/"  # 临时文件夹路径
+    temp_crop_name = temp_path + "crop.png"  # 需擦除矩形部分临时图片
+    temp_image_name = temp_path + "image.png"  # 全水印临时图片
+    temp_zoom_image_name = temp_path + "zoomImage.png"  # 调整大小后的无水印图路径
+    temp_crop_irregular_name = temp_path + "cropIrregular.png"  # 需擦除不规则部分黑背景临时图片
+    temp_crop_irregular_transparent_name = temp_path + "cropIrregularTransparent.png"  # 需擦除不规则部分透明背景临时图片
 
     # 打开并转换图片和水印
     image_file = Image.open(image_path)
@@ -29,12 +36,10 @@ def add_image_mark(image_path, mark_path, args):
         new_size = (int(watermark_x * watermark_scale), int(watermark_y * watermark_scale))
 
     # 若类型为全图水印，且cover为1，则进行图片大小调整
-    if (args.type == 0 or args.type == 2) and args.cover == 1:
-        print(1)
+    if (args.type == 0 or args.type == 2 or args.type == 3) and args.cover == 1:
         new_size = (int(watermark_x), int(watermark_y))
         rgba_image = rgba_image.resize(new_size, resample=Image.ANTIALIAS)
 
-        temp_zoom_image_name = temp_path + "zoomImage.png"  # 调整大小后的无水印图路径
         rgba_image.save(temp_zoom_image_name, quality=args.quality)
         image_file = Image.open(temp_zoom_image_name)
     else:
@@ -75,8 +80,6 @@ def add_image_mark(image_path, mark_path, args):
         up = int(index_array[1])
         right = int(index_array[2])
         down = int(index_array[3])
-        temp_crop_name = temp_path + "crop.png"  # 需擦除部分临时图片
-        temp_image_name = temp_path + "image.png"  # 全水印临时图片
 
         # 若临时文件夹不存在则新建
         if not os.path.exists(temp_path):
@@ -101,6 +104,45 @@ def add_image_mark(image_path, mark_path, args):
         temp_images = temp_file.convert("RGBA")
         temp_images.paste(crop_images, (int(left), int(up), int(right), int(down)))
         image = temp_images
+    elif args.type == 3:  # 剪切不规则图形
+        # 若临时文件夹不存在则新建
+        if not os.path.exists(temp_path):
+            os.mkdir(temp_path)
+
+        # 读取图片文件
+        img = cv2.imread(temp_zoom_image_name)
+        channels = img.shape[2]
+        mask = np.zeros(img.shape, dtype=np.uint8)
+        # 按每点坐标划线进行不规则剪切，TODO 遍历args.points得到每点坐标
+        roi_corners = np.array([[(100, 100), (200, 200), (700, 800), (50, 900)]], dtype=np.int32)
+        channels_count = channels
+        ignore_mask_color = (255,) * channels_count
+        cv2.fillPoly(mask, roi_corners, ignore_mask_color)
+        masked_image = cv2.bitwise_and(img, mask)
+        # 保存图片文件
+        cv2.imwrite(temp_crop_irregular_name, masked_image)
+        cv2.waitKey()
+
+        black_image = Image.open(temp_crop_irregular_name)
+        black_image_rgba = black_image.convert("RGBA")
+        data_array = black_image_rgba.getdata()
+        new_data = list()
+        for item in data_array:
+            if item[0] <= 10 or item[1] <= 10 or item[2] <= 10:
+                new_data.append((255, 255, 255, 0))
+            else:
+                new_data.append(item)
+        black_image_rgba.putdata(new_data)
+        black_image_rgba.save(temp_crop_irregular_transparent_name, quality=args.quality)
+        temp_crop_irregular_transparent_img = Image.open(temp_crop_irregular_transparent_name)
+        black_irregular_transparent_rgba = temp_crop_irregular_transparent_img.convert("RGBA")
+
+        image.save(temp_image_name, quality=args.quality)
+        temp_file = Image.open(temp_image_name)
+        temp_images = temp_file.convert("RGBA")
+        temp_images.show()
+        temp_images.paste(black_irregular_transparent_rgba, (0, 0))
+        image = temp_images
 
     mark_name = os.path.basename(mark_path)
     pre_name = mark_name.split(".")[0]
@@ -121,6 +163,7 @@ def add_image_mark(image_path, mark_path, args):
     else:
         print(file_name + " ---> 失败")
 
+
 def main():
     parse = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
     parse.add_argument("-f", "--file", type=str, default="./images",
@@ -130,11 +173,13 @@ def main():
     parse.add_argument("-o", "--out", type=str, default="./output",
                        help="请输入一个目录或图像文件路径，默认为./output")
     parse.add_argument("-t", "--type", default=0, type=int,
-                       help="请输入水印类型（0：全图水印，1：局部水印，2：全图水印擦除部门），默认为0")
+                       help="请输入水印类型（0：全图水印，1：局部水印，2：全图水印擦除矩形部分，3：全图水印擦除不规则部分），默认为0")
     parse.add_argument("-c", "--cover", default=0, type=int,
                        help="当类型为0或2时，若水印失真，则将图片大小调整为水印大小，默认为0，不作调整")
     parse.add_argument("-r", "--range", default="0,0,100,100", type=str,
                        help="若type为2，则请输入需擦除的矩形范围（左，上，右，下）, 默认为：0,0,100,100")
+    parse.add_argument("-p", "--points", default="50,100,200,300", type=str,
+                       help="若type为3，则请输入需擦除的点坐标（每两个为一组坐标）, 默认为：50,100,200,300")
     parse.add_argument("-l", "--location", default="0", type=str,
                        help="若为局部水印请输入水印位置（0：左上，1：左下，2：右上，3：右下，4：随机），若需自定义则填写开始坐标如：0,0, 默认为:0")
     parse.add_argument("-opa", "--opacity", default=0.15, type=float,
